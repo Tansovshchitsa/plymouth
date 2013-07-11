c5c81b21418e76df664d1afc5923c52ccc1a68f4
丁历祥 <dlx_swpu@qq.com>
2013-07-11 23:56:13 +0800

    enroll validates

--- app/controllers/enrolls_controller.rb
+++ app/controllers/enrolls_controller.rb
@@ -3,20 +3,7 @@
 class EnrollsController < ApplicationController
   load_and_authorize_resource
 
-  def index
-    @enrolls = Enroll.all
-  end
-
-  def show
-    @enroll = Enroll.find(params[:id])
-  end
-
-  def new
-    
-  end
-
-  def edit
-    @enroll = Enroll.find(params[:id])
+  def new  
   end
 
   def create
@@ -25,31 +12,8 @@ class EnrollsController < ApplicationController
     if @enroll.save
       redirect_to news_path(@enroll.news), :notice => "报名成功！"
     else
-      redirect_to news_path(@enroll.news), :notice => "报名失败！请核对填写信息。"
+      redirect_to news_path(@enroll.news), :notice => "#{@enroll.errors[:email]} #{@enroll.errors[:username]} #{@enroll.errors[:tel]}"
     end
   end
 
-  def update
-    @enroll = Enroll.find(params[:id])
-
-    respond_to do |format|
-      if @enroll.update_attributes(params[:enroll])
-        format.html { redirect_to @enroll, notice: 'Enroll was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @enroll.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @enroll = Enroll.find(params[:id])
-    @enroll.destroy
-
-    respond_to do |format|
-      format.html { redirect_to enrolls_url }
-      format.json { head :no_content }
-    end
-  end
 end
