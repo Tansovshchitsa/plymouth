823e23b4f844fda123dbfc64abd6dd3615295b38
txz <281040844@qq.com>
2013-07-15 11:07:43 +0800

    Revert "客户端校验"
    This reverts commit 9bde0c74215b43b11ed4a86637077b75bda2f51c.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require rails.validations
-//= require rails.validations.simple_form
 //= require ckeditor/init
 //= require_tree .
